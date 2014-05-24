4aa2df20c29d982c81941d3c2d69bccb653e6935
Robert de Bath <rdebath@tvisiontech.co.uk>
2014-05-24 10:15:09 +0100

    Clean up VT52 leftovers from the 256 colours patch
    Some references to the unused flag vt52_bold are removed.
    
    Remove the messing with the 'blink_is_real' flag as it's no longer
    needed for the Atari colours now the flag no longer overlaps with the
    bright background.
    
    Add the MiNT blink codes now the the flag will work.
    
    Also, the wrap mode should be set to "2" for Atari compatibility but
    only if it's set by the vt52 command as the VT100 sets a vt100 style wrap.

--- terminal.c
+++ terminal.c
@@ -2780,13 +2780,6 @@ static void toggle_mode(Terminal *term, int mode, int query, int state)
 	    break;
 	  case 2:		       /* DECANM: VT52 mode */
 	    term->vt52_mode = !state;
-	    if (term->vt52_mode) {
-		term->blink_is_real = FALSE;
-		term->vt52_bold = FALSE;
-	    } else {
-		term->blink_is_real = term->blinktext;
-	    }
-	    term_schedule_tblink(term);
 	    break;
 	  case 3:		       /* DECCOLM: 80/132 columns */
 	    deselect(term);
@@ -3303,7 +3296,7 @@ static void term_display_graphic_char(Terminal *term, unsigned long c)
     if (term->curs.x == term->cols) {
         term->curs.x--;
         term->wrapnext = TRUE;
-        if (term->wrap && term->vt52_mode) {
+        if (term->wrap == 2) {
             cline->lattr |= LATTR_WRAPPED;
             if (term->curs.y == term->marg_b)
                 scroll(term, term->marg_t, term->marg_b, 1, TRUE);
@@ -5159,8 +5152,6 @@ static void term_out(Terminal *term)
 		     *     emulation.
 		     */
 		    term->vt52_mode = FALSE;
-		    term->blink_is_real = term->blinktext;
-		    term_schedule_tblink(term);
 		    break;
 #if 0
 		  case '^':
@@ -5234,9 +5225,17 @@ static void term_out(Terminal *term)
 		    /* compatibility(ATARI) */
 		    term->curr_attr &= ~ATTR_REVERSE;
 		    break;
+		  case 'r':
+		    /* compatibility(ATARI) MiNT */
+		    term->curr_attr |= ATTR_BLINK;
+		    break;
+		  case 's':
+		    /* compatibility(ATARI) MiNT */
+		    term->curr_attr &= ~ATTR_BLINK;
+		    break;
 		  case 'v':	       /* wrap Autowrap on - Wyse style */
 		    /* compatibility(ATARI) */
-		    term->wrap = 1;
+		    term->wrap = 2;
 		    break;
 		  case 'w':	       /* Autowrap off */
 		    /* compatibility(ATARI) */
@@ -5245,7 +5244,6 @@ static void term_out(Terminal *term)
 
 		  case 'R':
 		    /* compatibility(OTHER) */
-		    term->vt52_bold = FALSE;
 		    term->curr_attr = ATTR_DEFAULT;
                     term->curr_truecolour.fg = optionalrgb_none;
                     term->curr_truecolour.bg = optionalrgb_none;
@@ -5261,12 +5259,10 @@ static void term_out(Terminal *term)
 		    break;
 		  case 'U':
 		    /* compatibility(VI50) */
-		    term->vt52_bold = TRUE;
 		    term->curr_attr |= ATTR_BOLD;
 		    break;
 		  case 'T':
 		    /* compatibility(VI50) */
-		    term->vt52_bold = FALSE;
 		    term->curr_attr &= ~ATTR_BOLD;
 		    break;
 #endif
@@ -5285,7 +5281,6 @@ static void term_out(Terminal *term)
 	      case VT52_FG:
 		term->termstate = TOPLEVEL;
 		term->curr_attr &= ~ATTR_FGMASK;
-		term->curr_attr &= ~ATTR_BOLD;
 		term->curr_attr |= (c & 0xF) << ATTR_FGSHIFT;
 		term->curr_truecolour.fg.enabled = FALSE;
 		set_erase_char(term);
@@ -5293,7 +5288,6 @@ static void term_out(Terminal *term)
 	      case VT52_BG:
 		term->termstate = TOPLEVEL;
 		term->curr_attr &= ~ATTR_BGMASK;
-		term->curr_attr &= ~ATTR_BLINK;
 		term->curr_attr |= (c & 0xF) << ATTR_BGSHIFT;
 		term->curr_truecolour.bg.enabled = FALSE;
 		set_erase_char(term);
